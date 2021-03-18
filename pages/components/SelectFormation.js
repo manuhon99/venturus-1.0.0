77a9ee4a75321f50dcc9961c38fe85ea230f1b37
manuhon99 <manuhon99@gmail.com>
2021-03-18 16:45:03 -0300

    select formation file with react-select and svg patterns

--- pages/components/SelectFormation.js
+++ pages/components/SelectFormation.js
@@ -1,24 +1,32 @@
 import React from 'react';
 import Select from 'react-select';
+import styles from "../../styles/components/SelectFormation.module.css";
 
 const options = [
+  { value: '3-2-2-3', label: '3-2-2-3' },
+  { value: '3-2-3-2', label: '3-2-3-2' },
   { value: '3-4-3', label: '3-4-3' },
-  { value: '4-4-3', label: '4-4-3' },
+  { value: '3-5-2', label: '3-5-2' },
+  { value: '4-2-3-1', label: '4-2-3-1' },
+  { value: '4-3-2-1', label: '4-3-2-1' },
+  { value: '4-3-3', label: '4-3-3' },
   { value: '4-4-2', label: '4-4-2' },
+  { value: '4-5-1', label: '4-5-1' },
+  { value: '5-4-1', label: '5-4-1' },
 ];
 
 
 const List = ({formation}) => (
   <div>
-  <img src={`/taticFormation/${formation}.svg`} alt="player"/>
+  <img src={`/taticFormation/${formation}.svg`} alt="tatic"/>
   </div>
 );
 
-let formation = null
+let formation = '3-4-3'
 
 class App extends React.Component {
   state = {
-    selectedOption: null,
+    selectedOption:  { value: '3-4-3', label: '3-4-3' },
   };
   handleChange = selectedOption => {
     this.setState({ selectedOption });
@@ -31,13 +39,17 @@ class App extends React.Component {
 
     return (
      
-      <div>
-      <Select
-        value={selectedOption}
-        onChange={this.handleChange}
-        options={options}
-      />
-     <List  formation={formation}></List>
+      <div className={styles.formation}>Formation
+        <Select className={styles.select}
+          value={selectedOption}
+          onChange={this.handleChange}
+          options={options}
+          defaultValue='3-4-3'
+        />
+      
+      <div className={styles.field}>
+      <List  formation={formation}></List>
+      </div>
        
       </div>
 
--- pages/newteam.js
+++ pages/newteam.js
@@ -125,24 +125,7 @@ export default function NewTeam({data}) {
 
         <div className={styles.configureSquadContainer}>
           <div className={styles.formation}>
-            <label htmlFor="formation">Formation   
             <App></App>
-              <select name="formatioon" id="formatioon">
-                <option value="3-2-2-3">3 - 2 - 2 - 3</option>
-                <option value="3-2-3-1">3 - 2 - 3 - 1</option>
-                <option value="3-4-3">3 - 4 - 3</option>
-                <option value="3-5-2">3 - 5 - 2</option>
-                <option value="4-2-3-1">4 - 2 - 3 - 1</option>
-                <option value="4-3-2">4 - 3 - 2</option>
-                <option value="4-4-2">4 - 4 - 2</option>
-                <option value="4-5-1">4 - 5 - 1</option>
-                <option value="5-4-1">5 - 4 -1</option>       
-              </select>
-            </label>
-            
-            <div className={styles.field}>
-              <img src="/taticFormation/3-4-3.svg" className={styles.img} alt="player"/>
-            </div>
             <button>Save</button>
           </div>
           <div className={styles.players}>           
