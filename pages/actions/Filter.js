51727c519f045a8c99866ad762d896cdc9f9b99b
manuhon99 <manuhon99@gmail.com>
2021-03-20 15:03:00 -0300

    testing swtich style on drag li elements

---
+++ pages/actions/Filter.js
@@ -0,0 +1,105 @@
+import React, { useState } from "react";
+import Draggable from "react-draggable";
+import { actions } from "react-table";
+import styles from "../../styles/components/CreateNewTeam.module.css";
+
+
+export default function Filter({data}) {
+
+  const [isactive, setActive] = useState(false);
+
+  const eventControl = (event, info) => {
+    console.log('Event name: ', event.type);
+    console.log(event, info);
+  }
+  const state = {
+    deltaXyPos: {
+      x: 0, 
+      y: 0
+    }
+  };
+  let position = null
+  
+  const handleDrag = (e, d) => {
+    const { x, y } = state.deltaXyPos;
+    const setState = ({
+      deltaXyPos: {
+        x: x + d.deltaX,
+        y: y + d.deltaY,
+      }
+    });
+    position=setState
+    console.log(position);  
+    setActive(!isactive)
+    
+  };
+  
+  //takes the value from the user and saves it into the state
+  const [searchTerm, setSearchTerm] = React.useState("");
+  //set the search result
+  const [searchResults, setSearchResults] = React.useState([]);
+  //takes the event object as the arguement and sets the current value of the 
+  //form to the searchTerm state using setSearchTerm method provided by 
+  //React.useState method
+  const handleChange = event => {
+     setSearchTerm(event.target.value);
+   };
+   //whenever the value of the dependencies in the React.useEffect hook 
+   //changes the function in its first argument execute
+  React.useEffect(() => {
+    //if the name in the names's list includes the searchTerm then return 
+    //true otherwise return false
+     const results = data.filter(filtered =>
+       filtered.toLowerCase().includes(searchTerm.toLowerCase())
+     );
+     setSearchResults(results);
+   }, [searchTerm]);
+
+   return (
+     <div className="App">
+      <input
+        type="text"
+        placeholder="Search"
+        value={searchTerm}
+        onChange={handleChange}
+        className={styles.inputFilter}
+      />
+      <ul>  
+        {data.map(item => (
+          <div className={styles.playersList}>
+            <div className={styles.nameNacionality}>
+              <li>Name: {item}</li>
+              <li>Nacionality: {item}</li>
+            </div>
+            <div className={styles.age}>
+              <li>Age: {item}</li>
+            </div>
+          </div>
+        ))
+        }
+      </ul>
+      <ul>
+         
+         {searchTerm &&
+          (searchResults.map(item => (
+            
+            <Draggable
+            onStart={eventControl}
+            onDrag={handleDrag}
+            >
+            <div onDrag={() => setActive(isactive)} className={ isactive ? styles.playersList : styles.playericon}>
+              <div className={styles.nameNacionality}>
+                <li>Name: {item}</li>
+                <li>Nacionality: {item}</li>
+              </div>
+              <div className={styles.age}>
+                <li>Age: {item}</li>
+              </div>
+            </div>
+            </Draggable>
+          )))
+            }
+      </ul>
+     </div>
+   );
+ }
--- pages/newteam.js
+++ pages/newteam.js
@@ -5,128 +5,20 @@ import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Example from "./components/Tag";
 import App from "./actions/SelectFormation";
-import Draggable from "react-draggable";
 import { createLocalStorageStateHook } from "use-local-storage-state";
+import Filter from "./actions/Filter";
 
 export const useTodos = createLocalStorageStateHook("todos");
 
-const MyModule = () => {
-  const [isactive, setActive] = useState(false);
-
-
-  return (
-    
-    <button onClick={() => setActive(!isactive)} >
-      <h1 name='myComponent' style={{background: isactive ? 'red' : 'blue'}}>
-        State: {isactive}
-      </h1>
-    </button>
-  )
-}
-
 export default function NewTeam({data}) {
 
-  const eventControl = (event, info) => {
-    console.log('Event name: ', event.type);
-    console.log(event, info);
-  }
-  const state = {
-    deltaXyPos: {
-      x: 0, 
-      y: 0
-    }
+  const [todos, setTodos] = useTodos();
+  const [todo, setTodo] = useState("");
+
+  const onClick = () => {
+    setTodos([...todos, todo]);
+    setTodo("");
   };
-  let position = null
-  let choice = styles.playersList
-  const handleDrag = (e, d) => {
-    const { x, y } = state.deltaXyPos;
-    const setState = ({
-      deltaXyPos: {
-        x: x + d.deltaX,
-        y: y + d.deltaY,
-      }
-    });
-    position=setState
-    console.log(position);
-    choice=styles.playericon
-    console.log(choice)
-    
-    
-  };
-  
-  
-
-  function Filter() {
-    //takes the value from the user and saves it into the state
-    const [searchTerm, setSearchTerm] = React.useState("");
-    //set the search result
-    const [searchResults, setSearchResults] = React.useState([]);
-    //takes the event object as the arguement and sets the current value of the 
-    //form to the searchTerm state using setSearchTerm method provided by 
-    //React.useState method
-    const handleChange = event => {
-       setSearchTerm(event.target.value);
-     };
-     //whenever the value of the dependencies in the React.useEffect hook 
-     //changes the function in its first argument execute
-    React.useEffect(() => {
-      //if the name in the names's list includes the searchTerm then return 
-      //true otherwise return false
-       const results = data.filter(filtered =>
-         filtered.toLowerCase().includes(searchTerm.toLowerCase())
-       );
-       setSearchResults(results);
-     }, [searchTerm]);
-
-     
-   
-     return (
-     
-       <div className="App">
-         <input
-           type="text"
-           placeholder="Search"
-           value={searchTerm}
-           onChange={handleChange}
-           className={styles.inputFilter}
-         />
-         <ul>
-           
-           {searchTerm &&
-            (searchResults.map(item => (
-              
-              
-              <Draggable
-              onStart={eventControl}
-              onDrag={handleDrag}
-
-              >
-            <div className={choice}>
-              
-              <div className={styles.nameNacionality}>
-                <li>Name: {item}</li>
-                <li>Nacionality: {item}</li>
-              </div>
-              <div className={styles.age}>
-                <li>Age: {item}</li>
-              </div>
-            </div>
-            </Draggable>
-           )))
-            }
-         </ul>
-       </div>
-     );
-   }
-
-const [todos, setTodos] = useTodos();
-const [todo, setTodo] = useState("");
-
-const onClick = () => {
-  setTodos([...todos, todo]);
-  setTodo("");
-};
-
 
   return (
     <div>
@@ -147,7 +39,6 @@ const onClick = () => {
 
         <div className={styles.titleArea}>
         <h1>TEAM INFORMATION</h1>
-        <MyModule></MyModule>
         </div>
         
 
@@ -199,8 +90,9 @@ const onClick = () => {
           </div>
           <div className={styles.players}>           
             <label htmlFor="search">Search Players</label>
+            
 
-            <Filter/>
+            <Filter data={data}></Filter>
             <hr></hr>
             
           </div>          
@@ -226,7 +118,7 @@ NewTeam.getInitialProps = async (ctx) => {
 
   let list = []
   let jsonList = []
-  for(let i=1;i<5;i++){
+  for(let i=1;i<4;i++){
     let id=i
     const res = await fetch(`https://api.football-data.org/v2/players/${id}`, requestOptions)
     const json = await res.json()
