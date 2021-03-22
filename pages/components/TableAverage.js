6a915f4168cb9eeac2c7690ca65f9ff288d8664b
manuhon99 <manuhon99@gmail.com>
2021-03-22 00:36:28 -0300

    test

--- pages/components/TableAverage.js
+++ pages/components/TableAverage.js
@@ -1,4 +1,4 @@
-import styles from "../../styles/components/TableAvarage.module.css";
+import styles from '../../styles/components/TabledSavedTeams.module.css';
 
 export default function TableAverage(){
 
